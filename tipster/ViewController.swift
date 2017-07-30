377da6d7b8adc789c7d4e3d9063e6c859e07e2bc
Ryan Zhang <ryan@ghost.local>
2017-07-29 18:39:11 -0700

    Initial Commit

---
+++ tipster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipster
+//
+//  Created by Ryan Zhang on 7/29/17.
+//  Copyright © 2017 Ryan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
